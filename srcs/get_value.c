b9f8a583e10b13ed61ae66098b7a4aa043b91c5a
Junit38 <Junit38@gmail.com>
2020-09-23 12:19:27 +0200

    norm

--- srcs/get_value.c
+++ srcs/get_value.c
@@ -6,11 +6,29 @@
 /*   By: mery <mery@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/15 14:51:02 by jmery             #+#    #+#             */
-/*   Updated: 2020/09/22 16:50:25 by mery             ###   ########.fr       */
+/*   Updated: 2020/09/23 12:18:34 by mery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "computor_v2.h"
+
+double		get_func_value_2(t_var *finded, char *func_var, t_param *param)
+{
+	double	value;
+
+	if (ft_strcmp(param->value, finded->func_var) == 0)
+	{
+		value = find_value(func_var);
+		if (value != 0)
+			return (recursive_power(value, param->power));
+		return (recursive_power(atof(func_var), param->power));
+	}
+	else if (!is_digit(param->value) && param->value[0] != '-')
+		return (find_value(param->value));
+	else if (param->sym == 0 && param->value && param->value[0])
+		return (recursive_power(atof(param->value), param->power));
+	return (0);
+}
 
 double		get_func_value(t_var *finded, char *func_var, t_param *param)
 {
@@ -32,25 +50,38 @@ double		get_func_value(t_var *finded, char *func_var, t_param *param)
 	else if (param->sym == '/')
 		value = get_func_value(finded, func_var, param->left)
 		/ get_func_value(finded, func_var, param->right);
-	else if (ft_strcmp(param->value, finded->func_var) == 0)
-	{
-		value = find_value(func_var);
-		if (value != 0)
-			return (recursive_power(value, param->power));
-		return (recursive_power(atof(func_var), param->power));
-	}
-	else if (!is_digit(param->value) && param->value[0] != '-')
-		return (find_value(param->value));
-	else if (param->sym == 0 && param->value && param->value[0])
-		return (recursive_power(atof(param->value), param->power));
+	else if (value == 0)
+		return (get_func_value_2(finded, func_var, param));
 	return (recursive_power(value, param->power));
+}
+
+t_var		*find_value_2(char *value, char **func_var, t_var *curs)
+{
+	char		*name;
+	t_var		*finded;
+
+	finded = NULL;
+	name = clean_line(to_lower_case(value));
+	if (is_func(name))
+	{
+		if (*func_var)
+			free(*func_var);
+		*func_var = get_func_var(name);
+		name = get_func_name(name);
+		if (ft_strcmp(name, curs->name) == 0)
+			finded = curs;
+		if (name)
+			free(name);
+	}
+	else if (ft_strcmp(name, curs->name) == 0)
+		finded = curs;
+	return (finded);
 }
 
 double		find_value(char *value)
 {
 	t_var		*curs;
 	t_var		*finded;
-	char		*name;
 	char		*func_var;
 	double		val;
 
@@ -59,20 +90,7 @@ double		find_value(char *value)
 	func_var = NULL;
 	while (curs && !finded)
 	{
-		name = clean_line(to_lower_case(value));
-		if (is_func(name))
-		{
-			if (func_var)
-				free(func_var);
-			func_var = get_func_var(name);
-			name = get_func_name(name);
-			if (ft_strcmp(name, curs->name) == 0)
-				finded = curs;
-			if (name)
-				free(name);
-		}
-		else if (ft_strcmp(name, curs->name) == 0)
-			finded = curs;
+		finded = find_value_2(value, &func_var, curs);
 		curs = curs->next;
 	}
 	if (finded && func_var)
