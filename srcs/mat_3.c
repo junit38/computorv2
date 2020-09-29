60c7a779b04c117098befd877b45766c55c055aa
Junit38 <Junit38@gmail.com>
2020-09-29 17:44:59 +0200

    norm

--- srcs/mat_3.c
+++ srcs/mat_3.c
@@ -6,7 +6,7 @@
 /*   By: mery <mery@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/15 14:51:02 by jmery             #+#    #+#             */
-/*   Updated: 2020/09/28 14:27:07 by mery             ###   ########.fr       */
+/*   Updated: 2020/09/29 17:44:51 by mery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,31 +19,28 @@ double		**multiply_mat_param(t_param *param)
 	double		**mat2;
 
 	mat = NULL;
-	if (param && param->left && param->right->mat
-		&& param->right && param->left->value)
+	if (!param || !param->left || !param->right)
+		return (NULL);
+	if (param->right->mat && param->left->value)
 	{
-		a = atof(param->left->value);
 		mat = param->right->mat;
+		a = atof(param->left->value);
 		mat = multiply_mat_2(a, mat, param->right->mat_len);
 	}
-	else if (param && param->left && param->left->mat
-		&& param->right && param->right->value)
+	else if (param->left->mat && param->right->value)
 	{
 		mat = param->left->mat;
 		a = atof(param->right->value);
 		mat = multiply_mat_2(a, mat, param->left->mat_len);
 	}
-	else if (param && param->left && param->left->mat && 
-		param->right && param->right->mat)
-	{
-		mat = param->left->mat;
-		mat2 = param->right->mat;
-		mat = multiply_mat(param->left->mat, param->left->mat_len, param->right->mat, param->right->mat_len);
-	}
+	else if (param->left->mat && param->right->mat && (mat = param->left->mat)
+		&& (mat2 = param->right->mat))
+		mat = multiply_mat(param->left->mat, param->left->mat_len,
+			param->right->mat, param->right->mat_len);
 	return (mat);
 }
 
-double 		**add_mat_param(t_param *param)
+double		**add_mat_param(t_param *param)
 {
 	double		**mat;
 	double		**mat2;
@@ -59,7 +56,7 @@ double 		**add_mat_param(t_param *param)
 	return (mat);
 }
 
-double 		**minus_mat_param(t_param *param)
+double		**minus_mat_param(t_param *param)
 {
 	double		**mat;
 	double		**mat2;
@@ -75,65 +72,58 @@ double 		**minus_mat_param(t_param *param)
 	return (mat);
 }
 
-double	 	**divide_mat_param(t_param *param)
+double 		**divide_mat_param(t_param *param)
 {
 	double		a;
 	double		**mat;
 	double		**mat2;
 
 	mat = NULL;
-	if (param && param->left && param->right->mat
-		&& param->right && param->left->value)
+	if (!param || !param->left || !param->right)
+		return (NULL);
+	if (param->right->mat && param->left->value)
 	{
 		a = atof(param->left->value);
 		mat = param->right->mat;
 		mat = divide_mat_2(a, mat, param->right->mat_len);
 	}
-	else if (param && param->left && param->left->mat
-		&& param->right && param->right->value)
+	else if (param->left->mat && param->right->value)
 	{
 		mat = param->left->mat;
 		a = atof(param->right->value);
 		mat = divide_mat_2(a, mat, param->left->mat_len);
 	}
-	else if (param && param->left && param->left->mat && 
-		param->right && param->right->mat)
-	{
-		mat = param->left->mat;
-		mat2 = param->right->mat;
-		mat = divide_mat(param->left->mat, param->left->mat_len, param->right->mat, param->right->mat_len);
-	}
+	else if (param->left->mat && param->right->mat && (mat = param->left->mat)
+		&& (mat2 = param->right->mat))
+		mat = divide_mat(param->left->mat, param->left->mat_len,
+			param->right->mat, param->right->mat_len);
 	return (mat);
 }
 
-double	 	**modulo_mat_param(t_param *param)
+double		**modulo_mat_param(t_param *param)
 {
 	double		a;
 	double		**mat;
 	double		**mat2;
 
 	mat = NULL;
-	if (param && param->left && param->right->mat
-		&& param->right && param->left->value)
+	if (!param || !param->left || !param->right)
+		return (NULL);
+	if (param->right->mat && param->left->value)
 	{
 		a = atof(param->left->value);
 		mat = param->right->mat;
 		mat = modulo_mat_2(a, mat, param->right->mat_len);
 	}
-	else if (param && param->left && param->left->mat
-		&& param->right && param->right->value)
+	else if (param->left->mat && param->right->value)
 	{
 		mat = param->left->mat;
 		a = atof(param->right->value);
 		mat = modulo_mat_2(a, mat, param->left->mat_len);
 	}
 	else if (param && param->left && param->left->mat && 
-		param->right && param->right->mat)
-	{
-		mat = param->left->mat;
-		mat2 = param->right->mat;
+		param->right && param->right->mat && (mat = param->left->mat)
+		&& (mat2 = param->right->mat))
 		mat = modulo_mat(param->left->mat, param->left->mat_len, param->right->mat, param->right->mat_len);
-	}
-	return (mat);
 	return (mat);
 }
