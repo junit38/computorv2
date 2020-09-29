3439b608fd7a7e3902a10b6180922ef690ca465d
Junit38 <Junit38@gmail.com>
2020-09-29 14:39:47 +0200

    norm

--- srcs/resolve_3.c
+++ srcs/resolve_3.c
@@ -6,16 +6,29 @@
 /*   By: mery <mery@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/15 14:51:02 by jmery             #+#    #+#             */
-/*   Updated: 2020/09/29 13:26:19 by mery             ###   ########.fr       */
+/*   Updated: 2020/09/29 14:39:42 by mery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "computor_v2.h"
 
-void	reduce_mat(t_param *param)
+void	reduce_mat_2(t_param *param)
 {
 	double	**mat;
 	int		mat_len;
+
+	mat = get_mat(param);
+	mat_len = get_mat_len_param(param);
+	clean_param(param);
+	param->mat = mat;
+	param->mat_len = mat_len;
+	param->left = NULL;
+	param->right = NULL;
+	param->sym = 0;
+}
+
+void	reduce_mat(t_param *param)
+{
 	char	*value;
 
 	if (param->left && param->right && (((param->left->value
@@ -23,16 +36,7 @@ void	reduce_mat(t_param *param)
 		(param->right->value && is_digit(param->right->value)
 			&& param->left->mat)) || ((param->left && param->right
 		&& param->left->mat && param->right->mat))))
-	{
-		mat = get_mat(param);
-		mat_len = get_mat_len_param(param);
-		clean_param(param);
-		param->mat = mat;
-		param->mat_len = mat_len;
-		param->left = NULL;
-		param->right = NULL;
-		param->sym = 0;
-	}
+		reduce_mat_2(param);
 	else if (param->value && find_var(param->value) != NULL)
 	{
 		value = ft_ftoa(get_value(param));
@@ -111,13 +115,13 @@ int		can_be_reduced(t_param *param)
 	return (ret);
 }
 
-int			can_be_resolved(t_param *param)
+int		can_be_resolved(t_param *param)
 {
 	int		ret;
 
 	ret = 0;
-
-	if (param && param->value && !is_digit(param->value) && param->value[0] != '-')
+	if (param && param->value && !is_digit(param->value)
+		&& param->value[0] != '-')
 	{
 		if (find_var(param->value))
 			ret = 1;
