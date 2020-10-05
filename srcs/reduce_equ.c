7f3d91536c7fc478fd8ba5d7e3213243c2c6a770
Junit38 <Junit38@gmail.com>
2020-10-05 17:39:26 +0200

    norm

--- srcs/reduce_equ.c
+++ srcs/reduce_equ.c
@@ -6,13 +6,13 @@
 /*   By: mery <mery@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/15 14:51:02 by jmery             #+#    #+#             */
-/*   Updated: 2020/10/05 17:32:26 by mery             ###   ########.fr       */
+/*   Updated: 2020/10/05 17:38:24 by mery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "computor_v2.h"
 
-static int		get_b_img(t_param *param)
+static int	get_b_img(t_param *param)
 {
 	if (is_img_squared(param))
 		return (get_val_img(param, 0) - get_val_noimg(param, 0));
@@ -21,7 +21,7 @@ static int		get_b_img(t_param *param)
 	return (get_val_img(param, 1) - get_val_noimg(param, 1));
 }
 
-static int		get_a_img(t_param *param)
+static int	get_a_img(t_param *param)
 {
 	int		img;
 
@@ -37,31 +37,35 @@ static int		get_a_img(t_param *param)
 	return (get_val_noimg(param, 1));
 }
 
-t_param			*reduce_equ(t_param *param, int freeparam)
+void		reduce_equ_2(t_param *param, t_param *new_param)
+{
+	int			val;
+
+	new_param->right = init_param();
+	new_param->right->right = init_param();
+	new_param->right->left = init_param();
+	new_param->right->right->value = ft_strdup("1");
+	new_param->right->right->isimg = 1;
+	new_param->right->right->sym = '*';
+	new_param->right->left->value = ft_itoa(get_b_img(param));
+	val = atoi(new_param->right->left->value);
+	if (val < 0)
+	{
+		free(new_param->right->left->value);
+		new_param->right->left->value = ft_itoa(val * -1);
+		new_param->sym = '-';
+	}
+	else
+		new_param->sym = '+';
+}
+
+t_param		*reduce_equ(t_param *param, int freeparam)
 {
 	t_param		*new_param;
-	int			val;
 
 	new_param = init_param();
 	if (get_b_img(param) != 0)
-	{
-		new_param->right = init_param();
-		new_param->right->right = init_param();
-		new_param->right->left = init_param();
-		new_param->right->right->value = ft_strdup("1");
-		new_param->right->right->isimg = 1;
-		new_param->right->right->sym = '*';
-		new_param->right->left->value = ft_itoa(get_b_img(param));
-		val = atoi(new_param->right->left->value);
-		if (val < 0)
-		{
-			free(new_param->right->left->value);
-			new_param->right->left->value = ft_itoa(val * -1);
-			new_param->sym = '-';
-		}
-		else
-			new_param->sym = '+';
-	}
+		reduce_equ_2(param, new_param);
 	if (get_a_img(param) != 0 || get_b_img(param) == 0)
 	{
 		new_param->left = init_param();
